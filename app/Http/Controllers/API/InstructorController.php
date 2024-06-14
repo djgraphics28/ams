e1299e8ad337240f6388b069f46d641264ef3531
Darwin Ibay <darwin.i@shoredigitalinc.com>
2024-06-14 22:13:30 +0800

    uupdates

--- app/Http/Controllers/API/InstructorController.php
+++ app/Http/Controllers/API/InstructorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class InstructorController extends Controller
 {
