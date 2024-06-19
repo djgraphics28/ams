7e32a06507232a94a913f1e8c1bc9d5a14663963
djgraphics28 <darwin.ibay30@gmail.com>
2024-06-19 18:17:33 +0800

    updates

---
+++ database/migrations/2024_06_19_180551_add_parent_details_to_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->string('parent_name');
+            $table->string('parent_number')->nullable();
+            $table->string('parent_email')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropColumn('parent_name');
+            $table->dropColumn('parent_number');
+            $table->dropColumn('parent_email');
+        });
+    }
+};
