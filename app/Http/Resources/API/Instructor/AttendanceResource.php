690c01444df429b7eb346d64b5b4e79fdc9cd678
darwin ibay <darwin.i@shoredigitalinc.com>
2024-06-24 19:49:04 +0800

    updates

--- app/Http/Resources/API/Instructor/AttendanceResource.php
+++ app/Http/Resources/API/Instructor/AttendanceResource.php
@@ -23,9 +23,10 @@ class AttendanceResource extends JsonResource
 
         return [
             'id' => $this->id,
-            'time_in' => $time_in ? $time_in->format('h:i A') : null,
+            'time_in' => $time_in ? $time_in : null,
             'scanned_by' => $this->instructor->full_name ?? null,
             'schedule' => $this->schedule->sched_code ?? null,
+            'student_name' => $this->student->full_name ?? null,
             'start' => $start->format('h:i A'),
             'end' => Carbon::parse($this->schedule->end)->timezone('Asia/Manila')->format('h:i A') ?? null,
             'is_late' => $time_in ? $time_in->greaterThan($startToday) : false,
